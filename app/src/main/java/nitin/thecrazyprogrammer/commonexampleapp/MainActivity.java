46038fa5351b43936b3e7b092bb1d79aa4d378f2
nitin070895a <nitin070895@gmail.com>
2018-03-12 17:38:48 +0530

    Added button themes

--- app/src/main/java/nitin/thecrazyprogrammer/commonexampleapp/MainActivity.java
+++ app/src/main/java/nitin/thecrazyprogrammer/commonexampleapp/MainActivity.java
@@ -3,6 +3,9 @@ package nitin.thecrazyprogrammer.commonexampleapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import nitin.thecrazyprogrammer.common.NonAvailabilityHolder;
 
 /**
  * Created by Nitin on 10-03-2018.
@@ -14,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        NonAvailabilityHolder nonAvailabilityHolder = new NonAvailabilityHolder(this, findViewById(android.R.id.content));
+        nonAvailabilityHolder.setButton("Retry", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        nonAvailabilityHolder.setVisibility(View.VISIBLE);
     }
 }
